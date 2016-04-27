fe4bda39f71e97aa7ac5d44e6be61aa620326e58
Morjiai <Morjiai@gmail.com>
2016-04-27 01:08:41 -0400

    Bugs Fixed and Progress Bar [1.1]
    Code simplified, and bugs fixed. 
    1. Noticed I forgot wget when downloading Open-Source-M-Repositories
    2. I don't know why I didn't just cd to app and download framework7. But know it does that. 
    3. I removed 1.4.2 for some reason. Now the code doesn't do that. 
    Added Features
    1. Progress bar.

--- make.sh
+++ make.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-if [ `builtin type -p unzip` ]; then echo "Everything needed to use this program is installed! Moving on.."; else sudo apt-get install unzip; fi
-echo "Making"
+if 
+[ `builtin type -p unzip` ]; then echo "Everything needed to use this program is installed! Moving on.."; else sudo apt-get install unzip;
+fi
+echo -ne '#####                     (33%)\r'
+sleep 1
 mkdir app 
+cd app
 wget https://github.com/nolimits4web/Framework7/archive/v1.4.2.zip
+echo -ne '#############             (66%)\r'
+sleep 1
 unzip 1.4.2.zip -d app
-cd app/Framework7-1.4.2
-mv ../Framework7-1.4.2/* ../
-rm -rf Framework7-1.4.2
 rm -rf v1.4.2.zip
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
 read -r -p "Would you like to install Open-Source-M-Repositories? By ColeSchaefer [y/N] " response
 case $response in
     [yY][eE][sS]|[yY]) 
-       cd ../ && https://github.com/ColeSchaefer/Open-Source-M-Repositories/archive/master.zip && unzip master.zip -d app && rm -rf master.zip
+       wget https://github.com/ColeSchaefer/Open-Source-M-Repositories/archive/master.zip && unzip master.zip -d app && rm -rf master.zip
         
         ;;
     *)
         echo "Done!";
         ;;
 esac
+
